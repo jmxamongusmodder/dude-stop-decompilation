a79f99e23b24307981d9e160938b281510da88bb
jmxamongusmodder <80003646+jmxamongusmodder@users.noreply.github.com>
2023-12-28 13:33:25 -0500

    Implement spider and cat transition?

--- Assets/Scripts/Assembly-CSharp-firstpass/Spider.cs
+++ Assets/Scripts/Assembly-CSharp-firstpass/Spider.cs
@@ -1,12 +1,69 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
+// Token: 0x0200033C RID: 828
 public class Spider : MonoBehaviour
 {
-	public float speed;
+	// Token: 0x06001442 RID: 5186 RVA: 0x0003445D File Offset: 0x0003285D
+	private void Awake()
+	{
+		if (this.coroutine != null)
+		{
+			return;
+		}
+		this.coroutine = base.StartCoroutine(this.Move());
+	}
+
+	// Token: 0x06001443 RID: 5187 RVA: 0x00034480 File Offset: 0x00032880
+	private IEnumerator Move()
+	{
+		this.shift = UnityEngine.Random.value * 100f;
+		for (; ; )
+		{
+			this.shift += Time.deltaTime * this.speed;
+			float prog = Mathf.PerlinNoise(this.shift, 0f);
+			Vector3 pos = this.spider.localPosition;
+			pos.y = Mathf.Lerp(this.minHeight, this.maxHeight, prog);
+			this.spider.localPosition = pos;
+			Vector3 webPos = this.web.localPosition;
+			float dist = Mathf.Abs(this.webTop - pos.y);
+			webPos.y = this.webTop - dist * 0.5f;
+			this.web.localPosition = webPos;
+			Vector3 scale = this.web.localScale;
+			scale.y = dist * this.webScale;
+			this.web.localScale = scale;
+			yield return null;
+		}
+		yield break;
+	}
+
+	// Token: 0x0400116D RID: 4461
+	public float speed = 0.3f;
+
+	// Token: 0x0400116E RID: 4462
+	[Header("Spider")]
 	public Transform spider;
-	public float maxHeight;
-	public float minHeight;
+
+	// Token: 0x0400116F RID: 4463
+	public float maxHeight = 1f;
+
+	// Token: 0x04001170 RID: 4464
+	public float minHeight = 2f;
+
+	// Token: 0x04001171 RID: 4465
+	private float shift;
+
+	// Token: 0x04001172 RID: 4466
+	[Header("Web")]
 	public Transform web;
-	public float webTop;
-	public float webScale;
+
+	// Token: 0x04001173 RID: 4467
+	public float webTop = 1f;
+
+	// Token: 0x04001174 RID: 4468
+	public float webScale = 1.56f;
+
+	// Token: 0x04001175 RID: 4469
+	private Coroutine coroutine;
 }
--- Assets/Scripts/Assembly-CSharp-firstpass/StoryMode_TransitionCat.cs
+++ Assets/Scripts/Assembly-CSharp-firstpass/StoryMode_TransitionCat.cs
@@ -1,10 +1,122 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
+// Token: 0x02000471 RID: 1137
 public class StoryMode_TransitionCat : MonoBehaviour
 {
+	// Token: 0x06001D38 RID: 7480 RVA: 0x00080294 File Offset: 0x0007E694
+	private void Awake()
+	{
+		base.gameObject.SetActive(false);
+	}
+
+	// Token: 0x06001D39 RID: 7481 RVA: 0x000802A4 File Offset: 0x0007E6A4
+	public void showCat(Transform oldCat)
+	{
+		base.transform.SetParent(null);
+		base.transform.localScale = oldCat.lossyScale;
+		base.transform.position = oldCat.position;
+		base.transform.rotation = oldCat.rotation;
+		this.originalX = base.transform.position.x;
+		base.gameObject.SetActive(true);
+		base.StartCoroutine(this.moveCat());
+		this.rotationCoroutine = base.StartCoroutine(this.RotationCoroutine());
+		StoryMode_TransitionCat.self = this;
+	}
+
+	// Token: 0x06001D3A RID: 7482 RVA: 0x0008033A File Offset: 0x0007E73A
+	public void SetRotationPosition(Vector3 position)
+	{
+		this.lastPosition = position;
+	}
+
+	// Token: 0x06001D3B RID: 7483 RVA: 0x00080344 File Offset: 0x0007E744
+	private IEnumerator moveCat()
+	{
+		float time = Global.self.transitionTimeSide;
+		while (time != 0f)
+		{
+			time = Mathf.MoveTowards(time, 0f, Time.deltaTime);
+			float prog = time / Global.self.transitionTimeSide;
+			float curveValue = Global.self.transitionStoryMode.Evaluate(prog);
+			Vector2 pos = base.transform.position;
+			pos.x = this.originalX + (this.destinationX - this.originalX) * curveValue;
+			base.transform.position = pos;
+			yield return null;
+		}
+		while (Global.self.currPuzzle == null)
+		{
+			yield return null;
+		}
+		time = 0f;
+		float startingAngle = base.transform.eulerAngles.z;
+		base.StopCoroutine(this.rotationCoroutine);
+		while (time != this.rotationTime)
+		{
+			time = Mathf.MoveTowards(time, this.rotationTime, Time.deltaTime);
+			float angle = Mathf.LerpAngle(startingAngle, 0f, time / this.rotationTime);
+			base.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+			yield return null;
+		}
+		PuzzleCatOnBlackDress[] list = Global.self.currPuzzle.GetComponentsInChildren<PuzzleCatOnBlackDress>(true);
+		if (list.Length != 0)
+		{
+			list[0].gameObject.SetActive(true);
+		}
+		UnityEngine.Object.Destroy(base.gameObject);
+		StoryMode_TransitionCat.self = null;
+		yield break;
+	}
+
+	// Token: 0x06001D3C RID: 7484 RVA: 0x00080360 File Offset: 0x0007E760
+	private void RotateAnimal()
+	{
+		float num = (base.transform.localPosition.x - this.lastPosition.x) / this.length;
+		float z = Mathf.Sin(num * 3.1415927f * 0.5f) * this.amplitude;
+		base.transform.localRotation = Quaternion.Euler(0f, 0f, z);
+	}
+
+	// Token: 0x06001D3D RID: 7485 RVA: 0x000803CC File Offset: 0x0007E7CC
+	private IEnumerator RotationCoroutine()
+	{
+		yield return new WaitForEndOfFrame();
+		for (; ; )
+		{
+			this.RotateAnimal();
+			yield return null;
+		}
+		yield break;
+	}
+
+	// Token: 0x04001BDE RID: 7134
+	public static StoryMode_TransitionCat self;
+
+	// Token: 0x04001BDF RID: 7135
 	public float destinationX;
-	public float length;
-	public float amplitude;
-	public float rotationTime;
-	public float scaleTime;
+
+	// Token: 0x04001BE0 RID: 7136
+	private float originalX;
+
+	// Token: 0x04001BE1 RID: 7137
+	[Header("Rotation stuff")]
+	[Tooltip("Distance to walk for one of four cycles")]
+	public float length = 1f;
+
+	// Token: 0x04001BE2 RID: 7138
+	[Tooltip("Max angle for moving cat")]
+	public float amplitude = 10f;
+
+	// Token: 0x04001BE3 RID: 7139
+	[Tooltip("The time in which the cat rotates back to zero while not dragged")]
+	public float rotationTime = 0.5f;
+
+	// Token: 0x04001BE4 RID: 7140
+	public float scaleTime = 0.3f;
+
+	// Token: 0x04001BE5 RID: 7141
+	private Vector3 lastPosition;
+
+	// Token: 0x04001BE6 RID: 7142
+	private Coroutine rotationCoroutine;
 }
